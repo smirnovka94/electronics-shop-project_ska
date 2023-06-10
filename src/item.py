0072a1aa700a733371952771b84551142ff7d90c
kirill <lekseich8@gmail.com>
2023-06-10 13:42:54 +0300

    add Item.all.append(self)

--- src/item.py
+++ src/item.py
@@ -16,6 +16,9 @@ class Item:
         self.name = name
         self.price = price
         self.quantity = quantity
+        Item.all.append(self)
+
+
 
     def calculate_total_price(self) -> float:
         """
